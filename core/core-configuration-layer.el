c1d34765eeafbdad715c169133c4c189d72d2398
Chris Young <cygitmail@gmail.com>
2020-09-30 09:54:37 +0800

    Use Chinese mirror

--- core/core-configuration-layer.el
+++ core/core-configuration-layer.el
@@ -220,9 +220,9 @@ is not set for the given SLOT."
     (eval `(oset pkg ,slot value))))
 
 (defvar configuration-layer--elpa-archives
-  '(("melpa" . "melpa.org/packages/")
-    ("org"   . "orgmode.org/elpa/")
-    ("gnu"   . "elpa.gnu.org/packages/"))
+  '(("melpa" . "mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")
+    ("org"   . "mirrors.tuna.tsinghua.edu.cn/elpa/org/")
+    ("gnu"   . "mirrors.tuna.tsinghua.edu.cn/elpa/gnu/"))
   ;; '(("spacelpa" . "~/.emacs.d/.cache/spacelpa/"))
   "List of ELPA archives required by Spacemacs.")
 
@@ -2176,9 +2176,9 @@ Original code from dochang at https://github.com/dochang/elpa-clone"
   "Create an ELPA repository containing all packages supported by Spacemacs."
   (configuration-layer/make-all-packages 'no-discover)
   (let (package-archive-contents
-        (package-archives '(("melpa" . "https://melpa.org/packages/")
-                            ("org"   . "https://orgmode.org/elpa/")
-                            ("gnu"   . "https://elpa.gnu.org/packages/"))))
+        (package-archives '(("melpa" . "https://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")
+                            ("org"   . "https://mirrors.tuna.tsinghua.edu.cn/elpa/org/")
+                            ("gnu"   . "https://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/"))))
     (package-refresh-contents)
     (package-read-all-archive-contents)
     (let* ((packages (configuration-layer//get-indexed-elpa-package-names))
